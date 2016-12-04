3c1218b5d9cb22de6a17d0d7e8c23664803afd09
myark <g.ark.zone@gmail.com>
2016-12-04 13:19:38 +0800

    添加navigatorIOS

--- index.js
+++ index.js
@@ -2,18 +2,25 @@ import React, { Component } from 'react';
 import {
   View,
   Text,
+  NavigatorIOS,
 } from 'react-native';
+
+import Home from './home';
 
 export default class App extends Component {
   constructor(props) {
     super(props);
   }
-
   render() {
     return (
-      <View>
-        <Text>hello</Text>
-      </View>
+      <NavigatorIOS
+        initialRoute={{
+          component: Home,
+          title: 'v2ex',
+          passProps: this.props
+        }}
+        style={{flex: 1}}
+      />
     )
   }
 }
